ee169b012285b7b9cf565cae4adb88d3d8c78d0d
graciano <15754324647@163.com>
2019-12-18 15:49:49 +0800

    commit test

--- src/components/Registration.vue
+++ src/components/Registration.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-<!--    <label>-->
-<!--      用户名称：<input type="text" v-model="userName" aria-placeholder="请输入用户名"/>-->
-<!--    </label>-->
-    <label>用户姓名：<el-input v-model="realName" placeholder="请输入姓名" size="medium"></el-input></label>
-    <label>用户昵称：<el-input v-model="nick" placeholder="请输入昵称" size="medium"></el-input></label>
+    <!--    <label>-->
+    <!--      用户名称：<input type="text" v-model="userName" aria-placeholder="请输入用户名"/>-->
+    <!--    </label>-->
+    <label>用户姓名：
+      <el-input v-model="realName" placeholder="请输入姓名" size="medium"></el-input>
+    </label>
+    <label>用户昵称：
+      <el-input v-model="nick" placeholder="请输入昵称" size="medium"></el-input>
+    </label>
     <el-button type="primary" @click="register()">注册</el-button>
   </div>
 
@@ -15,19 +19,21 @@
     name: "Registration",
     data() {
       return {
-        realName: ""
+        realName: "",
+        nick: ""
       }
     },
     methods: {
       register() {
         console.log(this.realName)
+        console.log(this.nick)
       }
     }
   }
 </script>
 
 <style scoped>
-.el-input{
-  width: 10%;
-}
+  .el-input {
+    width: 10%;
+  }
 </style>
